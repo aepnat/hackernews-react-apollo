5555522716106652e4757f7c0ff98ea39f2134c2
Saepul Bahri <aep.nat@gmail.com>
2020-11-24 05:44:40 +0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import '../styles/App.css';
+import LinkList from "./LinkList";
+import React from "react";
+import CreateLink from "./CreateLink";
+import Header from "./Header";
+import {Route, Switch} from "react-router";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </div>
+  );
+}
+
+export default App;
